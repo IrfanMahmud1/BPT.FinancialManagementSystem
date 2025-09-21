efb83a5d2d5d3e1f0641608dbc94b96b8a98cc6e
Irfan Mahmud <99061976+IrfanMahmud1@users.noreply.github.com>
2025-09-21 06:10:50 +0600

    Add project files.

---
+++ src/BPT.FMS/BPT.FMS.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BPT.FMS.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
